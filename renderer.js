9cc62dfd94ddd5b2368878501835f26368710c69
Arilson1 <arilsontere18@hotmail.com>
2022-07-14 18:09:11 -0300

    pega os dados / agradecimento

--- renderer.js
+++ renderer.js
@@ -1,14 +1,23 @@
 const { shell } = require('electron')
 const os = require('os')
 
-const message = document.getElementById('message').value
+const message = document.getElementById('message')
+const grato = document.getElementById('grato')
+const app = document.getElementById('app')
+
+grato.style.display = "none"
 
 content = {
+    message: "",
     hostname: os.hostname(),
     userinfo: os.userInfo(),
-    message: "" + message.value,
 }
 
 document.getElementById('openTicket').addEventListener('click', (event) => {
+    content.message = message.value
     console.log(content)
+    app.style.display = "none"
+    grato.style.display = "flex"
 })
+
+
